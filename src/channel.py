ef5e0338f43651f784cf927caf97e719adec3d0f
ivan kova1 <ivan.2002aus@gmail.com>
2024-09-15 15:20:21 +0300

    home_work_1 Аналитика YouTube

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,17 @@
-
-
+from googleapiclient.discovery import build
+import json
+import os
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        api_key: str = os.getenv('YT_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         pass
+
+c = Channel('AIzaSyBpls0q5DFRSc6Nr6ZIIEvhjOYYi7yVH7s"')
