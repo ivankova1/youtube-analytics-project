914013bf8a788c0d6a0eccdbc9ed29ec0e19aa75
ivan kova1 <ivan.2002aus@gmail.com>
2024-09-16 10:18:01 +0300

    improvements

--- src/channel.py
+++ src/channel.py
@@ -7,11 +7,10 @@ class Channel:
     youtube = build('youtube', 'v3', developerKey=api_key)
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        api_key: str = os.getenv('YT_API_KEY')
-        youtube = build('youtube', 'v3', developerKey=api_key)
-
+        self.__channel_id = channel_id
+        self.__channel = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        print(self.channel)
+        print(self.__channel)
 
-c = Channel('AIzaSyBpls0q5DFRSc6Nr6ZIIEvhjOYYi7yVH7s"')
+#c = Channel('AIzaSyBpls0q5DFRSc6Nr6ZIIEvhjOYYi7yVH7s"')
